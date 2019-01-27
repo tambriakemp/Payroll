f8691473f7929aedbfd3c8ce02c7030b4d47c155
Tambria Kemp <info@cre8visions.com>
2019-01-26 19:59:59 -0500

    added cors

--- app.js
+++ app.js
@@ -24,6 +24,13 @@ app.use('/users', usersRouter);
 app.use('/employees', employeesRouter); 
 app.use('/dependents', dependentsRouter); 
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    next();
+});
+
 
  
 // Database setup
