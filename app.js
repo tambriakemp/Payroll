8f22722e0f1cf83eb54d56aca8533d7cdda2af63
Tambria Kemp <info@cre8visions.com>
2019-01-26 20:08:00 -0500

    no message

--- app.js
+++ app.js
@@ -24,12 +24,12 @@ app.use('/users', usersRouter);
 app.use('/employees', employeesRouter); 
 app.use('/dependents', dependentsRouter); 
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+//     next();
+// });
 
 
  
