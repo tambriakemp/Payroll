69f3af062ac48719021cb2760b1f10f2d1f6e199
Tambria Kemp <info@cre8visions.com>
2019-01-26 20:05:10 -0500

    no message

--- routes/employees.js
+++ routes/employees.js
@@ -13,12 +13,23 @@ router.get('/', function(req, res) {
             console.error(err); 
             res.status(500).json({message: 'Internal Server Error'})
           });
-      
 });
 
-//Get all Employees Page
+//Get single employee
+router.get(':id', function(req, res) {
+  
+  Employee.findById(req.params.id)
+    .then(Employee => res.json(employee.serialize()))
+    .catch(err => {
+          console.error(err); 
+          res.status(500).json({message: 'Internal Server Error'})
+        });
+
+});
+
 
 //Add Employee 
+
 
 //Delete Employee 
 
@@ -26,7 +37,17 @@ router.get('/', function(req, res) {
 
 
 
-
+// app.get("/restaurants/:id", (req, res) => {
+//   Restaurant
+//     // this is a convenience method Mongoose provides for searching
+//     // by the object _id property
+//     .findById(req.params.id)
+//     .then(restaurant => res.json(restaurant.serialize()))
+//     .catch(err => {
+//       console.error(err);
+//       res.status(500).json({ message: "Internal server error" });
+//     });
+// });
 
 
 module.exports = router;  
